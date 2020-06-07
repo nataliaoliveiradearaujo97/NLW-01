db386e2d6959d36e089dc391dc163eef20db8a8c
Natália <40809517+nataliaoliveiradearaujo97@users.noreply.github.com>
2020-06-07 01:07:15 -0300

    Adicionando tryCatch

--- server/src/controllers/ItemsController.ts
+++ server/src/controllers/ItemsController.ts
@@ -3,17 +3,21 @@ import knex from '../database/connection';
 
 class ItemsController {
     async index (request: Request, response: Response) {
-        const items = await knex('items').select('*');
+        try {
+            const items = await knex('items').select('*');
 
-        const serializedItems = items.map(item => {
-            return {
-                id: item.id,
-                title: item.title,
-                image_url: `http://localhost:3333/uploads/${item.image}`
-            }
-        })
+            const serializedItems = items.map(item => {
+                return {
+                    id: item.id,
+                    title: item.title,
+                    image_url: `http://localhost:3333/uploads/${item.image}`
+                }
+            })
 
         return response.json(serializedItems);
+        } catch (error) {
+            console.log(error);
+        }
     }
 }
 
--- server/src/controllers/PersonalController.ts
+++ server/src/controllers/PersonalController.ts
@@ -3,98 +3,110 @@ import knex from '../database/connection';
 
 class PersonalController {
     async index (request: Request, response: Response) {
-        //cidade, uf, items (query params)
-        const { cidade, uf, items } = request.query;
+        try {
+            //cidade, uf, items (query params)
+            const { cidade, uf, items } = request.query;
 
-        const itemsId = String(items)
-        .split(',')
-        .map(item => Number(item.trim()));
+            const itemsId = String(items)
+            .split(',')
+            .map(item => Number(item.trim()));
 
-        const personal = await knex('personal')
-            .join('personal_items', 'personal.personal_id', '=', 'personal_items.personal_id')
-            .whereIn('personal_items.item_id', itemsId)
-            .where('cidade', String(cidade))
-            .where('uf', String(uf))
-            .distinct()
-            .select('personal.*');
+            const personal = await knex('personal')
+                .join('personal_items', 'personal.id', '=', 'personal_items.id')
+                .whereIn('personal_items.item_id', itemsId)
+                .where('cidade', String(cidade))
+                .where('uf', String(uf))
+                .distinct()
+                .select('personal.*');
 
-        return response.json(personal);
+            return response.json(personal);
+        } catch (error) {
+            console.log(error);
+        }
     }
     
     async show (request: Request, response: Response) {
-        const { id } = request.params;
+        try {
+            const { id } = request.params;
 
-        const personals = await knex('personal').where('id', id).first();
+            const personals = await knex('personal').where('id', id).first();
 
-        if(!personals) {
-            return response.status(400).json({ message: 'Personal não encontrado' });
+            if(!personals) {
+                return response.status(400).json({ message: 'Personal não encontrado' });
+            }
+
+            /**
+             * select * from personal p
+             * inner join items i on i.idItems = p.idItems
+             * where p.idItems = {id}
+            */
+            const items = await knex('items')
+                .join('personal_items', 'items.id', '=', 'personal_items.id')
+                .where('personal_items.id', id)
+                .select('items.nome');
+
+            return response.json({ personals, items });
+        } catch (error) {
+            console.log(error);
         }
-
-        /**
-         * select * from personal p
-         * inner join items i on i.idItems = p.idItems
-         * where p.idItems = {id}
-        */
-        const items = await knex('items')
-            .join('personal_items', 'items.id', '=', 'personal_items.items_id')
-            .where('personal_items.personal_id', id)
-            .select('items.nome');
-
-        return response.json({ personals, items });
     }
 
     async create (request: Request, response: Response) {
-        const {
-            nome,
-            nascimento,
-            sexo,
-            cpf,
-            metodologia,
-            whatsapp,
-            latitude,
-            longitude,
-            cidade,
-            uf,
-            email,
-            items
-        } = request.body;
-
-        const trx = await knex.transaction();
-
-        const personal = {
-            image: 'https://sportsjob.com.br/wp-content/uploads/2018/06/707.jpg',
-            nome,
-            nascimento,
-            sexo,
-            cpf,
-            metodologia,
-            whatsapp,
-            latitude,
-            longitude,
-            cidade,
-            uf,
-            email
-        }
-
-        const insertedIds = await trx('personal').insert(personal);
-
-        const personal_id = insertedIds[0];
-
-        const personalItems = await items.map((item_id: number) => {
-            return {
-                item_id,
-                personal_id
+        try {
+            const {
+                nome,
+                nascimento,
+                sexo,
+                cpf,
+                metodologia,
+                whatsapp,
+                latitude,
+                longitude,
+                cidade,
+                uf,
+                email,
+                items
+            } = request.body;
+    
+            const trx = await knex.transaction();
+    
+            const personal = {
+                image: 'https://sportsjob.com.br/wp-content/uploads/2018/06/707.jpg',
+                nome,
+                nascimento,
+                sexo,
+                cpf,
+                metodologia,
+                whatsapp,
+                latitude,
+                longitude,
+                cidade,
+                uf,
+                email
             }
-        })
-
-        await trx('personal_items').insert(personalItems);
-
-        await trx.commit();
-
-        return response.json({ 
-            id: personal_id,
-            ...personal,
-        })
+    
+            const insertedIds = await trx('personal').insert(personal);
+    
+            const personal_id = insertedIds[0];
+    
+            const personalItems = await items.map((item_id: number) => {
+                return {
+                    item_id,
+                    personal_id
+                }
+            })
+    
+            await trx('personal_items').insert(personalItems);
+    
+            await trx.commit();
+    
+            return response.json({ 
+                id: personal_id,
+                ...personal,
+            })
+        } catch (error) {
+            console.log(error);
+        }
     }
 }
 
